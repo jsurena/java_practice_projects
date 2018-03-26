a3933b52c8b1ad6e5976e4827da649aab3510327
Jean-Sebastien Surena <jsurena3652@bths.edu>
2018-03-25 21:49:33 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,15 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jean-Sebastien Surena");
+
+        int myFirstNum = (10 + 5) + (2 *10),
+        mySecondNum = 12,
+        myThirdNum = myFirstNum * 2;
+
+        int myTotal = myFirstNum + mySecondNum + myThirdNum;
+
+        int myLastOne = 1000 - myTotal;
+
+        System.out.println(myLastOne);
+    }
+}
