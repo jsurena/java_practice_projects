e9b8a8e60f493fd3b2568e8e6fd8f2f105652d1a
Jean-Sebastien Surena <jsurena3652@bths.edu>
2018-04-03 11:09:10 -0400

    Add files via upload

---
+++ Classes-Car.java
@@ -0,0 +1,47 @@
+package com.company;
+
+public class Car {
+
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String color;
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+    public void setWheels(int wheels) {
+        this.wheels = wheels;
+    }
+    public void setModel(String model) {
+        String validModel = model.toLowerCase();
+        if (validModel.equals("carrera") || validModel.equals("commodore")) {
+            this.model = model;
+        } else {
+            this.model = "Unknown";
+        }
+    }
+    public void setEngine(String engine) {
+        this.engine = engine;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getDoors() {
+        return this.doors;
+    }
+    public int getWheels() {
+        return this.wheels;
+    }
+    public String getModel() {
+        return this.model;
+    }
+    public String getEngine() {
+        return this.engine;
+    }
+    public String getColor() {
+        return this.color;
+    }
+}
